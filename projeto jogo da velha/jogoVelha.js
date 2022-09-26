3af9962a379c7446bd55946da44802ff2d8fa030
Thaís Felício de Paula Silva <83433694+Thaisfelicio@users.noreply.github.com>
2022-09-26 17:22:07 -0300

    Add files via upload

---
+++ projeto jogo da velha/jogoVelha.js
@@ -0,0 +1,4 @@
+const cellElements = document.querySelectorAll("[data-cell]");
+const board = document.querySelector("[data-board]");
+const winningMessageTextElement = document.querySelector("[data-winning-message-text]");
+const winning
