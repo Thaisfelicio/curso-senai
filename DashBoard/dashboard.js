ac51e640a73bf287dcdbee974ca4a234dfa4aa6e
Thaís Felício de Paula Silva <83433694+Thaisfelicio@users.noreply.github.com>
2022-10-24 17:26:14 -0300

    termino do dashboard
    Inserção do conteúdo da tabela através de javascript, o dashboard não é totalmente responsivo.

--- DashBoard/dashboard.js
+++ DashBoard/dashboard.js
@@ -4,50 +4,53 @@ const closeBtn = document.querySelector("#close-btn");
 const themeToggler = document.querySelector(".theme-toggler");
 
 //MOSTRAR SIDEBAR
-menuBtn.addEventListener("click", () =>{
-    sideMenu.style.display = "block";
+menuBtn.addEventListener("click", () => {
+  sideMenu.style.display = "block";
 });
 //FECHAR SIDEBAR
-closeBtn.addEventListener("click", () =>{
-    sideMenu.style.display = "none";
+closeBtn.addEventListener("click", () => {
+  sideMenu.style.display = "none";
 });
 //MUDAR TEMA
 themeToggler.addEventListener("click", () => {
-    document.body.classList.toggle("dark-theme-variables");
-    themeToggler.querySelector("span:nth-child(1)").classList.toggle("active");
-    themeToggler.querySelector("span:nth-child(2)").classList.toggle("active");
-  });
+  // o toggle serve pra trocar o estado do elemento como um interruptor
+  document.body.classList.toggle("dark-theme-variables");
+  themeToggler.querySelector("span:nth-child(1)").classList.toggle("active");
+  themeToggler.querySelector("span:nth-child(2)").classList.toggle("active");
+});
 //PREENCHER PEDIDOS NA TABELA
 let table = document.querySelector("table tbody");
 Orders.forEach((order) => {
-    // cria uma linha na tabela
-    let linha = document.createElement("tr");
-    console.log(order);
+  // cria uma linha na tabela
+  let linha = document.createElement("tr");
+  console.log(order);
 
-    let conteudo = 
-    "<td>" + order.productName + "</td>" +
-    "<td>" + order.productNumber + "</td>" +
+  let conteudo =
+    "<td>" +
+    order.productName +
+    "</td>" +
+    "<td>" +
+    order.productNumber +
+    "</td>" +
     ("<td class=" +
-    // aqui o nome da classe vai mudar de acordo com resultado dos operadores ternários, que são if's e else's
-    (order.shipping == "Negado"
-    // aqui muda o nome da classe para danger
-    ? "danger"
-    : order.shipping == "Pendente" 
-    // aqui muda o nome da classe para warning
-    ? "warning" 
-    // aqui muda o nome da classe para primary
-    : "primary") +
-    ">" + 
-    order.shipping + 
-    "</td>") + 
-    '<td class="primary">' + 
-    "Detalhes" + 
+      // aqui o nome da classe vai mudar de acordo com resultado dos operadores ternários, que são if's e else's
+      (order.shipping == "Negado"
+        ? // aqui muda o nome da classe para danger
+          "danger"
+        : order.shipping == "Pendente"
+        ? // aqui muda o nome da classe para warning
+          "warning"
+        : // aqui muda o nome da classe para primary
+          "primary") +
+      ">" +
+      order.shipping +
+      "</td>") +
+    '<td class="primary">' +
+    "Detalhes" +
     "</td>";
 
-    linha.innerHTML = conteudo;
-    table.appendChild(linha);
+  linha.innerHTML = conteudo;
+  // appendChild serve para adicionar um elemento filho ao elemento, o append só adciona, mas não se sabe quem o pai daquele
+  // elemento, appendChild diz Explicitamente que é o filho, aqui esta adicionando o elemento linha no table
+  table.appendChild(linha);
 });
-
-
-  
-  
---
+++ DashBoard/pedidos.js
@@ -0,0 +1,32 @@
+const Orders = [
+  {
+    productName: "Foldable Mini Drone",
+    productNumber: "85631",
+    paymentStatus: "Vencido",
+    shipping: "Pendente",
+  },
+  {
+    productName: "LARVENDER KF102 Drone",
+    productNumber: "36378",
+    paymentStatus: "reembolsado",
+    shipping: "Negado",
+  },
+  {
+    productName: "Ruko F11 Pro Drone",
+    productNumber: "49347",
+    paymentStatus: "Vencido",
+    shipping: "Negado",
+  },
+  {
+    productName: "Drone with Camera Drone",
+    productNumber: "96996",
+    paymentStatus: "Pago",
+    shipping: "Entregue",
+  },
+  {
+    productName: "GPS 4K Drone",
+    productNumber: "22821",
+    paymentStatus: "PAgo",
+    shipping: "Entregue",
+  },
+];
