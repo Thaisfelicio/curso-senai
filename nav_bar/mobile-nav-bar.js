cb7cadb594ae98b599e4abfb75d5b6a87dd1eec8
Thaís Felício de Paula Silva <83433694+Thaisfelicio@users.noreply.github.com>
2022-11-30 15:46:47 -0300

    atualização do arquivo javascript

---
+++ nav_bar/mobile-nav-bar.js
@@ -0,0 +1,31 @@
+class MobileNavbar{
+    constructor(mobileMenu, navList, navLinks){
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = "active";
+
+        this.handleClick = this.handleClick.bind(this);//o bind está criando um vinculo com a classe
+    }
+
+    animateLinks(){//adiciona animação em tempo de execução
+        this.navLinks.forEach((link, index) =>{
+            link.style.animation
+            ? (link.style.animation = "")
+            : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+                index / 7 + 0.3
+            }s`);
+        });
+    }
+    handleClick(){
+        this.navList.classList.toggle(this.activeClass);
+        this.mobileMenu.classList.toggle(this.activeClass);
+        this.animateLinks();
+    }
+
+    addClickEvent(){
+        this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+    
+}
+
